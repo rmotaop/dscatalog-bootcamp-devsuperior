574fea1532872e4f684b72cb7a3b2fcb9d5a2d74
rmotaop <rmotaop@gmail.com>
2021-07-26 19:02:16 -0300

    Add categories on product creation

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makePrivateRequest, makeRequest } from 'core/utils/request';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { toast } from 'react-toastify';
+import Select from 'react-select';
 import BaseForm from '../../BaseForm';
 import { useHistory, useParams } from 'react-router';
+import { Category } from 'core/types/Products';
 import './styles.scss';
 
 type FormState = {
@@ -11,6 +13,7 @@ type FormState = {
     price: string;
     description: string;
     imgUrl: string;
+    categories: Category[];
 }
 
 type ParamsType = {
@@ -18,12 +21,15 @@ type ParamsType = {
 }
 
 const Form = () => {
-    const { register, handleSubmit, errors, setValue } = useForm<FormState>();
+    const { register, handleSubmit, errors, setValue, control } = useForm<FormState>();
     const history = useHistory();
     const { productId } = useParams<ParamsType>();
+    const [isloadingCategories, setIsLoadingCategories] = useState(false);
+    const [categories, setCategories] = useState<Category[]>([]);
     const isEditing = productId !== 'create';
     const formTitle = isEditing ? 'Editar produto' : 'Cadastrar um produto';
 
+    
     useEffect(() => {
         if (isEditing) {
             makeRequest({ url: `/products/${productId}` })
@@ -31,11 +37,21 @@ const Form = () => {
                 setValue('name', response.data.name); 
                 setValue('price', response.data.price); 
                 setValue('description', response.data.description); 
-                setValue('imgUrl', response.data.imgUrl);    
+                setValue('imgUrl', response.data.imgUrl);
+                setValue('categories', response.data.categories);
             })
         }   
     }, [productId, isEditing, setValue]);
 
+    /* */
+    useEffect(() => { 
+          setIsLoadingCategories(true);
+          makeRequest({ url: '/categories' })
+            .then(response => setCategories(response.data.content)) 
+            .finally(() => setIsLoadingCategories(false));
+    }, []);
+
+    /* */
     const onSubmit = (data: FormState) => {
         makePrivateRequest({ 
             url: isEditing ? `/products/${productId}` : '/products', 
@@ -61,7 +77,7 @@ const Form = () => {
                         <div className="margin-botton-30">
                             <input
                                 ref={register({
-                                    required: "campo obrigatório",
+                                    required: "Campo obrigatório",
                                     minLength: {value: 5, message: 'O campo deve ter no mínimo 5 caracteres'},
                                     maxLength: {value: 60, message: 'O campo deve ter no máximo 60 caracteres'}
                                  })}
@@ -77,10 +93,31 @@ const Form = () => {
                             )}
                         </div>
 
+                        <div className="margin-botton-30"> 
+                            <Controller
+                                as={Select} 
+                                name="categories"
+                                rules={{required: true}}      
+                                control={control} 
+                                isLoading={isloadingCategories}
+                                options={categories} 
+                                getOptionLabel={(option: Category) => option.name}
+                                getOptionValue={(option: Category) => String(option.id)}
+                                classNamePrefix="categories-select"
+                                placeholder="Categorias"
+                                isMulti
+                            />    
+                                {errors.categories && (
+                                <div className="invalid-feedback d-block ">
+                                    Campo obrigatório
+                                </div>
+                            )}
+                        </div>
+
                         <div className="margin-botton-30">
                             <input
                                 ref={register({
-                                    required: "campo obrigatório",
+                                    required: "Campo obrigatório",
                                     min: {value: 0, message: 'O preço não pode ser negativo'}
                                 
                                 })}
@@ -99,7 +136,7 @@ const Form = () => {
                         
                         <div className="margin-botton-30">
                             <input
-                                ref={register({ required: "campo obrigatório" })}
+                                ref={register({ required: "Campo obrigatório" })}
                                 name="imgUrl"
                                 type="text"
                                 className="form-control input-base"
@@ -115,7 +152,7 @@ const Form = () => {
                     </div>
                     <div className="col-6">
                         <textarea
-                            ref={register({ required: "campo obrigatório" })}
+                            ref={register({ required: "Campo obrigatório" })}
                             name="description"
                             className="form-control input-base"
                             placeholder="Descrição"
